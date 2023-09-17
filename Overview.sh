0887077627684e0fa915e1b4cfec2212b18c2820
llorracc <git@llorracc.org>
2023-09-16 23:23:54 -0400

    autoupdate because main changed

--- Overview.sh
+++ Overview.sh
@@ -2,3 +2,6 @@
 source ~/.bash_profile ; make4ht --loglevel debug --utf8 --config Overview.cfg --format html5 Overview "svg" "-cunihtf -utf8"
 cat page-style.css | cat - Overview-generated-by-make4ht.css > Overview-page-style.css && mv Overview-page-style.css Overview.css
 cp Overview.html index.html
+source ~/.bash_profile ; make4ht --loglevel debug --utf8 --config Overview.cfg --format html5 Overview "svg"   "-cunihtf -utf8"
+cat page-style.css | cat - Overview-generated-by-make4ht.css > Overview-page-style.css && mv Overview-page-style.css Overview.css && rm page-style.css && rm Overview-generated-by-make4ht.css
+cp Overview.html index.html
