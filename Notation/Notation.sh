f6fafeda42b3edb1a3cb1a2b05f5d6b0a1f452a3
llorracc <git@llorracc.org>
2023-09-17 00:02:45 -0400

    autoupdate because main changed

--- Notation/Notation.sh
+++ Notation/Notation.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-source ~/.bash_profile ; make4ht --loglevel debug --utf8 --config Notation.cfg --format html5 Notation "svg" "-cunihtf -utf8"
-cat page-style.css | cat - Notation-generated-by-make4ht.css > Notation-page-style.css && mv Notation-page-style.css Notation.css
+source ~/.bash_profile ; make4ht --loglevel debug --utf8 --config Notation.cfg --format html5 Notation "svg"   "-cunihtf -utf8"
+cat page-style.css | cat - Notation-generated-by-make4ht.css > Notation-page-style.css && mv Notation-page-style.css Notation.css && rm page-style.css && rm Notation-generated-by-make4ht.css
 cp Notation.html index.html
