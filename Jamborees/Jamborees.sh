127be78b67f6ba506c92c996eb08ae46d26dbbce
llorracc <git@llorracc.org>
2023-09-24 16:10:28 -0400

    Fix jamborees

--- Jamborees/Jamborees.sh
+++ Jamborees/Jamborees.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-latexmk
-latexmk -c
+source ~/.bash_profile ; make4ht --loglevel debug --utf8 --config Jamborees.cfg --format html5 Jamborees "svg"   "-cunihtf -utf8"
+cat page-style.css | cat - Jamborees-generated-by-make4ht.css > Jamborees-page-style.css && mv Jamborees-page-style.css Jamborees.css && rm page-style.css && rm Jamborees-generated-by-make4ht.css
+cp Jamborees.html index.html
