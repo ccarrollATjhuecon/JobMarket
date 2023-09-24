aa30b423a0e2a8802c2b1e1dbb27d31e5cabd388
llorracc <git@llorracc.org>
2023-09-24 13:26:56 -0400

    autoupdate because main changed

--- Notation/Notation.sh
+++ Notation/Notation.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-latexmk
-latexmk -c
+source ~/.bash_profile ; make4ht --loglevel debug --utf8 --config Notation.cfg --format html5 Notation "svg"   "-cunihtf -utf8"
+cat page-style.css | cat - Notation-generated-by-make4ht.css > Notation-page-style.css && mv Notation-page-style.css Notation.css && rm page-style.css && rm Notation-generated-by-make4ht.css
+cp Notation.html index.html
