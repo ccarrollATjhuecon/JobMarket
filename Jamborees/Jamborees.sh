24ebf2b544f39986c5d8cb3b9624003fab37db44
ccarrollATjhuecon <git@ccarrollATjhhuecon>
2023-10-05 11:19:51 -0400

    autoupdate because main changed

--- Jamborees/Jamborees.sh
+++ Jamborees/Jamborees.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-source ~/.bash_profile ; make4ht --loglevel debug --utf8 --config Jamborees.cfg --format html5 Jamborees "svg"   "-cunihtf -utf8"
+source ~/.bash_profile ; make4ht --loglevel error --utf8 --config Jamborees.cfg --format html5 Jamborees "svg"   "-cunihtf -utf8"
 cat page-style.css | cat - Jamborees-generated-by-make4ht.css > Jamborees-page-style.css && mv Jamborees-page-style.css Jamborees.css && rm -f page-style.css && rm -f Jamborees-generated-by-make4ht.css
 cp Jamborees.html index.html
